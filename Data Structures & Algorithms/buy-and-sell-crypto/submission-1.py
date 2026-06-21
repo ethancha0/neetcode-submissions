66f4fc3cdb6ffa240f1621fc68495a1a6248e861
Ethan Chao <ethanchao2005@gmail.com>
2026-06-21 13:46:57 -0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        globalMin = prices[0] # assume min is first elem 
+        maxProfit = 0 
+        # find the max - min 
+        # but the max has to come after the min 
+
+        for elem in prices: 
+            globalMin = min(globalMin, elem)
+            maxProfit = max(maxProfit, elem-globalMin)
+        
+
+
+        return maxProfit
