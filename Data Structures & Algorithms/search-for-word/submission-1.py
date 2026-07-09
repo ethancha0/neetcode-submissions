9c0ed21da716dae39c7582a2444b8696dbfd4bfe
Ethan Chao <ethanchao2005@gmail.com>
2026-07-09 16:01:14 -0700

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS = len(board)
+        COLS = len(board[0])
+
+        visited = set()
+
+        def dfs(r, c, i) -> bool:
+            if i == len(word):
+                return True
+            # out of bounds (greater/less), invald letter, already searched
+            if(r >= ROWS or c >= COLS or 
+               r < 0 or c < 0 or 
+               board[r][c] != word[i] or 
+               (r,c) in visited):
+                return False
+
+            visited.add((r, c))
+
+            # recursive case
+            ans = (
+                dfs(r+1, c, i+1) or 
+                dfs(r-1, c, i+1) or 
+                dfs(r, c+1, i+1) or 
+                dfs(r, c-1, i+1) 
+            )
+
+            # backtrack 
+            visited.remove((r, c))
+
+            return ans
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):
+                    return True
+        return False
+
+
+
