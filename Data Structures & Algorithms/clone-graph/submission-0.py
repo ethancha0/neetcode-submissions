252eece1de25c51bd038024560c5d569253af3fd
Ethan Chao <ethanchao2005@gmail.com>
2026-07-11 18:23:59 -0700

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+
+        if not node:
+            return None
+
+        cloned = {}
+
+        def dfs(node): 
+            if node in cloned: 
+                return cloned[node]
+
+            c = Node(node.val)
+            cloned[node] = c 
+
+
+            for neighbor in node.neighbors:
+                c.neighbors.append(dfs(neighbor))
+
+            
+            return c
+
+        
+        return dfs(node)
+
+         
