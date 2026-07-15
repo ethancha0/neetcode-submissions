ef140777e4737d2820f592bbafe852bc6f821087
Ethan Chao <ethanchao2005@gmail.com>
2026-07-14 21:33:02 -0700

    Add: surrounded-regions - submission-3

---
+++ Data Structures & Algorithms/surrounded-regions/submission-3.py
@@ -0,0 +1,42 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS = len(board)
+        COLS = len(board[0])
+
+        def dfs(r, c):
+            if(
+                r < 0 or c < 0 or 
+                r >= ROWS or c >= COLS or 
+                board[r][c] != 'O'
+            ):
+                return 
+
+            board[r][c] = 'T'
+
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+
+        
+        #top/bottom rows 
+        for c in range(COLS): 
+            if board[0][c] == 'O':
+                dfs(0, c)
+            if board[ROWS-1][c] == 'O':
+                dfs(ROWS-1, c)
+        
+        #left/right walls 
+        for r in range(ROWS): 
+            if board[r][0] == 'O':
+                dfs(r, 0)
+            if board[r][COLS-1] == 'O':
+                dfs(r, COLS-1)
+
+        #final pass
+        for r in range(ROWS): 
+            for c in range(COLS):
+                if board[r][c] == 'T':
+                    board[r][c] = 'O'
+                elif board[r][c] == 'O':
+                    board[r][c] = 'X'
