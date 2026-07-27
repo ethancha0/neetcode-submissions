669e1e53a5ec1be1b24967efda649c22f5996bb6
Ethan Chao <ethanchao2005@gmail.com>
2026-07-27 13:54:00 -0700

    Add: spiral-matrix - submission-1

---
+++ Data Structures & Algorithms/spiral-matrix/submission-1.py
@@ -0,0 +1,41 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        #boundary walls of unexplored regions
+        left = 0
+        right = len(matrix[0])
+        top = 0 
+        bottom = len(matrix)
+
+        ans = []
+
+        while left < right and top < bottom:
+            #move left to right 
+            for i in range(left, right):
+                ans.append(matrix[top][i])
+            top += 1
+            
+            #move top to bottom
+            for i in range(top, bottom):
+                ans.append(matrix[i][right-1])
+            right -= 1 
+
+
+            if not (left < right and top < bottom):
+                break
+
+            
+            #move right to left 
+            for i in reversed(range(left, right)):
+                ans.append(matrix[bottom-1][i])
+            bottom -= 1 
+            
+            #move up 
+            for i in reversed(range(top, bottom)):
+                ans.append(matrix[i][left])
+            left += 1
+
+
+
+        return ans
+
+
