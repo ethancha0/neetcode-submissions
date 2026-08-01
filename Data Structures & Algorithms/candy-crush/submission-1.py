1eaf315b5510481705a4fdae7d26dae64d922aad
Ethan Chao <ethanchao2005@gmail.com>
2026-07-31 21:45:26 -0700

    Add: candy-crush - submission-1

---
+++ Data Structures & Algorithms/candy-crush/submission-1.py
@@ -0,0 +1,55 @@
+class Solution:
+    def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
+        ROWS = len(board)
+        COLS = len(board[0])
+
+        #searches each cell to see if in between vertically and horizontally.
+        def find():
+            findSet = set() 
+            #vertical check 
+            for r in range(1, ROWS-1): 
+                for c in range(COLS):
+                    if board[r][c] == 0: 
+                        continue
+                    if board[r][c] == board[r+1][c] and board[r][c] == board[r-1][c]:
+                        findSet.add((r, c))
+                        findSet.add((r+1, c))
+                        findSet.add((r-1, c))
+            #horizontal check 
+            for r in range(ROWS): 
+                for c in range(1, COLS-1): 
+                    if board[r][c] == 0:
+                        continue
+                    if board[r][c] == board[r][c+1] and board[r][c] == board[r][c-1]:
+                        findSet.add((r, c))
+                        findSet.add((r, c+1))
+                        findSet.add((r, c-1))
+            
+            return findSet 
+
+        #set all values to be crushed to be 0 
+        def crush(crushSet):
+            for r, c in crushSet:
+                board[r][c] = 0 
+
+        
+        def drop():
+            for c in range(COLS):
+                lowestEmpty = ROWS - 1
+                for r in reversed(range(ROWS)):
+                    #apply gravity when non-0 found
+                    if board[r][c] > 0:
+                        board[lowestEmpty][c] = board[r][c]
+                        lowestEmpty -= 1
+                #cleanup: everything above highest non-zero elem should be 0-ed
+                for r in range(lowestEmpty + 1):
+                    board[r][c] = 0
+
+        #continue with steps until no more crushable candies
+        crushedSet = find() 
+        while crushedSet: 
+            crush(crushedSet)
+            drop()
+            crushedSet = find()
+        
+        return board
